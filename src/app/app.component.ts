c2123a97d617b3b4b73f21945db45080a7fa07d3
Denis Kirilyuk <denkiril@gmail.com>
2024-10-06 14:16:36 +0300

    add eslint and prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'exokv-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'kvest-web';
-}
+export class AppComponent {}
